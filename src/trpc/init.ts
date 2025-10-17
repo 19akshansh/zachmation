6574d1e9394a17a9a079f8e1455c425f4f5b1dda
Akshansh Srivastava <akshanshsri.edu@gmail.com>
2025-10-17 12:48:29 +0530

    Update init.ts
    type safed initTRPC.create -> initTRPC.create.context()

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -12,7 +12,7 @@ export const createTRPCContext = cache(async () => {
 // since it's not very descriptive.
 // For instance, the use of a t variable
 // is common in i18n libraries.
-const t = initTRPC.create({
+const t = initTRPC.context<Awaited<ReturnType<typeof createTRPCContext>>>().create({
   /**
    * @see https://trpc.io/docs/server/data-transformers
    */
