f1217863160503f1694b5cc8ef023d99a0272186
Akshansh Srivastava <akshanshsri.edu@gmail.com>
2026-05-30 10:59:21 +0000

    Deployment Changes

--- src/app/(dashboard)/(rest)/subscriptions/page.tsx
+++ src/app/(dashboard)/(rest)/subscriptions/page.tsx
@@ -1,26 +1,7 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { useTRPC } from "@/trpc/client";
-import { useMutation } from "@tanstack/react-query";
-import { toast } from "sonner";
-
 const Page = () => {
-  const trpc = useTRPC();
-  const testAI = useMutation(trpc.testAi.mutationOptions({
-    onSuccess: () => {
-      toast.success("Success")
-    },
-    onError: () => {
-      toast.error("Failed")
-    }
-  }));
-
-  return (
-    <Button onClick={() => testAI.mutate()}>
-      Click to test sub
-    </Button>
-  )
-}
+  return <div>Subscription management coming soon.</div>;
+};
 
 export default Page;
